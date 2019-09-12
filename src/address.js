c9a229df8a7c4215f531067abc18c14bc708275d
Anhnt <anhntbk08@gmail.com>
2019-09-12 10:29:38 +0700

    [Chore] using babel for development and build

--- src/address.js
+++ src/address.js
@@ -13,8 +13,8 @@ const addressUtils = {};
  * @returns {string} Public key in hex string using ECDSA
  */
 addressUtils.privateKeyToPub = function (privKey) {
-  // Then generate the public point/key corresponding to your privKey.
-  return ECDSA.keyFromPrivate(privKey).getPublic().encodeCompressed('hex');
+    // Then generate the public point/key corresponding to your privKey.
+    return ECDSA.keyFromPrivate(privKey).getPublic().encodeCompressed('hex');
 };
 
 /**
@@ -25,30 +25,31 @@ addressUtils.privateKeyToPub = function (privKey) {
  * @returns {string} base58-check format for stealth address
  */
 addressUtils.generateAddress = function (pubSk, pubVk) {
-  const preAddr = pubSk + pubVk;
-  const hash = common.fastHash(preAddr);
-  const addrHex = preAddr + hash.slice(0, 8);
-  return Base58.encode(common.hextobin(addrHex));
+    const preAddr = pubSk + pubVk;
+    const hash = common.fastHash(preAddr);
+    const addrHex = preAddr + hash.slice(0, 8);
+    return Base58.encode(common.hextobin(addrHex));
 };
 
 /**
  * Generate keys for privacy base on secret key (also called private spend key)
- * @param {string} secret_key The wallet secret key of Tomochain account
- * @return {object} private spend key, private view key, public view key, public spend key, privacy address
+ * @param {string} secretKey The wallet secret key of Tomochain account
+ * @return {object} private spend key, private view key, public view key,
+ * public spend key, privacy address
  */
-addressUtils.generateKeys = function (secret_key) {
-  const privSk = secret_key;
-  const privVk = common.fastHash(privSk);
-  const pubSk = addressUtils.privateKeyToPub(privSk);
-  const pubVk = addressUtils.privateKeyToPub(privVk);
+addressUtils.generateKeys = function (secretKey) {
+    const privSk = secretKey;
+    const privVk = common.fastHash(privSk);
+    const pubSk = addressUtils.privateKeyToPub(privSk);
+    const pubVk = addressUtils.privateKeyToPub(privVk);
 
-  return {
-    privSpendKey: privSk,
-    pubSpendKey: pubSk,
-    privViewKey: privVk,
-    pubViewKey: pubVk,
-    pubAddr: addressUtils.generateAddress(pubSk, pubVk),
-  };
+    return {
+        privSpendKey: privSk,
+        pubSpendKey: pubSk,
+        privViewKey: privVk,
+        pubViewKey: pubVk,
+        pubAddr: addressUtils.generateAddress(pubSk, pubVk),
+    };
 };
 
 module.exports = addressUtils;
--- src/crypto.js
+++ src/crypto.js
@@ -1,4 +1,5 @@
-// TODO replace this module by third-party one, this is NATIVE LIB OF NODEJS, WOULD NOT RUN ON BROWSER
+// TODO replace this module by third-party one,
+// this is NATIVE LIB OF NODEJS, WOULD NOT RUN ON BROWSER
 const crypto = require('crypto');
 const ecurve = require('ecurve');
 
@@ -7,90 +8,90 @@ const ecparams = ecurve.getCurveByName('secp256k1');
 const BigInteger = ecparams.n.constructor;
 
 function hash160(buffer) {
-  buffer = crypto.createHash('sha256').update(buffer).digest();
-  return crypto.createHash('rmd160').update(buffer).digest();
+    const sha256 = crypto.createHash('sha256').update(buffer).digest();
+    return crypto.createHash('rmd160').update(sha256).digest();
 }
 
 function hmacSha256(buffer) {
-  return crypto.createHmac('sha256', new Buffer([])).update(buffer).digest();
+    return crypto.createHmac('sha256', new Buffer([])).update(buffer).digest();
 }
 
 function sha256x2(buffer) {
-  buffer = crypto.createHash('sha256').update(buffer).digest();
-  return crypto.createHash('sha256').update(buffer).digest();
+    const sha256 = crypto.createHash('sha256').update(buffer).digest();
+    return crypto.createHash('sha256').update(sha256).digest();
 }
 
 /**
  * Random a hex string with size bytes, no prefix 0x, add it yourself if needed
- * @param {number} Number of bytes, notice a hex = 4 bit = 0.5 byte, a common private key (length 64) = 32 bytes
+ * @param {number} Number of bytes, notice a hex = 4 bit = 0.5 byte,
+ * a common private key (length 64) = 32 bytes
  * @param {function} Callback
  * @returns {string} Hex string without prefix 0x
  */
 function randomHex(size, callback) {
-  const crypto = require('crypto') || window.crypto;
-  const isCallback = (typeof callback === 'function');
+    const cryto = crypto || window.crypto;
+    const isCallback = (typeof callback === 'function');
 
-
-  if (size > 65536) {
-    if (isCallback) {
-      callback(new Error('Requested too many random bytes.'));
-    } else {
-      throw new Error('Requested too many random bytes.');
-    }
-  }
-
-  // is node
-  if (typeof crypto !== 'undefined' && crypto.randomBytes) {
-    if (isCallback) {
-      crypto.randomBytes(size, (err, result) => {
-        if (!err) {
-          callback(null, result.toString('hex'));
+    if (size > 65536) {
+        if (isCallback) {
+            callback(new Error('Requested too many random bytes.'));
         } else {
-          callback(error);
+            throw new Error('Requested too many random bytes.');
         }
-      });
+    }
+
+    // is node
+    if (typeof cryto !== 'undefined' && cryto.randomBytes) {
+        if (isCallback) {
+            cryto.randomBytes(size, (err, result) => {
+                if (!err) {
+                    callback(null, result.toString('hex'));
+                } else {
+                    callback(err);
+                }
+            });
+        } else {
+            return cryto.randomBytes(size).toString('hex');
+        }
+
+        // is browser
     } else {
-      return crypto.randomBytes(size).toString('hex');
+        let crytoLib;
+
+        if (typeof cryto !== 'undefined') {
+            crytoLib = cryto;
+        } else if (typeof window.msCrypto !== 'undefined') {
+            crytoLib = window.msCrypto;
+        }
+
+        if (crytoLib && crytoLib.getRandomValues) {
+            const randomBytes = crytoLib.getRandomValues(new Uint8Array(size));
+            const returnValue = Array.from(randomBytes).map(arr => arr.toString(16)).join('');
+
+            if (isCallback) {
+                callback(null, returnValue);
+            } else {
+                return returnValue;
+            }
+
+            // not cryto object
+        } else {
+            const error = new Error('No "cryto" object available. This Browser doesn\'t support generating secure random bytes.');
+
+            if (isCallback) {
+                callback(error);
+            } else {
+                throw error;
+            }
+        }
     }
-
-    // is browser
-  } else {
-    let cryptoLib;
-
-    if (typeof crypto !== 'undefined') {
-      cryptoLib = crypto;
-    } else if (typeof msCrypto !== 'undefined') {
-      cryptoLib = msCrypto;
-    }
-
-    if (cryptoLib && cryptoLib.getRandomValues) {
-      const randomBytes = cryptoLib.getRandomValues(new Uint8Array(size));
-      const returnValue = Array.from(randomBytes).map(arr => arr.toString(16)).join('');
-
-      if (isCallback) {
-        callback(null, returnValue);
-      } else {
-        return returnValue;
-      }
-
-      // not crypto object
-    } else {
-      var error = new Error('No "crypto" object available. This Browser doesn\'t support generating secure random bytes.');
-
-      if (isCallback) {
-        callback(error);
-      } else {
-        throw error;
-      }
-    }
-  }
 }
 
 module.exports = {
-  BigInteger,
-  ecparams,
-  hash160,
-  hmacSha256,
-  sha256x2,
-  randomHex,
+    BigInteger,
+    ecparams,
+    hash160,
+    hmacSha256,
+    sha256x2,
+    randomHex,
 };

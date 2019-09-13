8bfc8ec4ad9235e04774c65412f2de52a8476511
Anhnt <anhntbk08@gmail.com>
2019-09-13 16:58:34 +0700

    [Feat] adding mask and encrypted amount for deposit input

--- test/end-to-end/deposit.js
+++ test/end-to-end/deposit.js
@@ -15,14 +15,15 @@ const expect = chai.expect;
 chai.should();
 
 const WALLETS = TestConfig.WALLETS;
+const SENDER_WALLET = WALLETS[0]; // hold around 1 mil tomo
 
 //load single private key as string
-let provider = new HDWalletProvider(WALLETS[0].privateKey, TestConfig.RPC_END_POINT);
+let provider = new HDWalletProvider(SENDER_WALLET.privateKey, TestConfig.RPC_END_POINT);
 
 const web3 = new Web3(provider);
 
 var privacyContract = new web3.eth.Contract(TestConfig.PRIVACY_ABI, TestConfig.PRIVACY_SMART_CONTRACT_ADDRESS, {
-    from: WALLETS[0].address, // default from address
+    from: SENDER_WALLET.address, // default from address
     gasPrice: '250000000', // default gas price in wei, 20 gwei in this case,
     gas: '1000000'
 });
@@ -32,17 +33,19 @@ describe('End to End Deposit to 1Tomo to SC', () => {
         let amount = 1000000000000000000; // 1 tomo
         // generate a tx 1 tomo from normal addess to privacy address
         let sender = new Stealth({
-            ...Address.generateKeys(WALLETS[0].privateKey)
+            ...Address.generateKeys(SENDER_WALLET.privateKey)
         })
 
         // create proof for a transaction 
         let proof = sender.genTransactionProof(amount, sender.pubSpendKey, sender.pubViewKey);
 
         privacyContract.methods.deposit(
-            Web3.utils.hexToNumberString(proof.onetimeAddress.slice(1).slice(0, 32).toString('hex')), 
-            Web3.utils.hexToNumberString(proof.onetimeAddress.slice(0, 1).toString('hex')),
-            Web3.utils.hexToNumberString(proof.txPublicKey.slice(1).slice(0, 32).toString('hex')), 
-            Web3.utils.hexToNumberString(proof.txPublicKey.slice(0, 1).toString('hex'))
+            Web3.utils.hexToNumberString(proof.onetimeAddress.toString('hex').substr(2, 64)), // the X part of curve 
+            Web3.utils.hexToNumberString(proof.onetimeAddress.toString('hex').substr(-64)), // the Y part of curve
+            Web3.utils.hexToNumberString(proof.txPublicKey.toString('hex').substr(2, 64)), // the X part of curve
+            Web3.utils.hexToNumberString(proof.txPublicKey.toString('hex').substr(-64)), // the Y par of curve,
+            Web3.utils.hexToNumberString(proof.mask),
+            Web3.utils.hexToNumberString(web3.utils.toHex(proof.encryptedAmount)) // encrypt of amount using ECDH
         )
             .send({
                 from: WALLETS[0].address,
@@ -55,14 +58,24 @@ describe('End to End Deposit to 1Tomo to SC', () => {
             .then(function(receipt){
                 try {
                     const UTXO = receipt.events.NewUTXO;
+                    console.log(UTXO)
                     UTXO.should.be.a('object')
                     expect(UTXO.transactionHash).to.have.lengthOf(66);
 
                     // double check the ownership
-                    
+                    /**
+                     * The UTXO structure 
+                     * commitmentX:
+                     * _commitmentYBit: '0',
+                     * _pubkeyX:
+                     * _pubkeyYBit: '1',
+                     * _amount: '39920883695728937215643250781821601832375934986290686600175182021887703344957',
+                     *_txPubX: ''
+                     *_txPubYBit: '0'
+                     */
                     let isMineUTXO = sender.checkTransactionProof(
-                        Buffer.from(web3.utils.numberToHex(UTXO.returnValues._pubkeyYBit + UTXO.returnValues._pubkeyX).slice(2), "hex"),
-                        Buffer.from(web3.utils.numberToHex(UTXO.returnValues._commitmentYBit + UTXO.returnValues._commitmentX).slice(2), "hex"),
+                        Buffer.from(web3.utils.numberToHex(UTXO.returnValues._pubkeyYBit + UTXO.returnValues._pubkeyX), "hex"),
+                        Buffer.from(web3.utils.numberToHex(UTXO.returnValues._commitmentYBit + UTXO.returnValues._commitmentX), "hex"),
                     )
                     expect(isMineUTXO).to.not.equal(null);
                     done();
--- test/unit/stealth.test.js
+++ test/unit/stealth.test.js
@@ -26,7 +26,8 @@ describe('stealth', function () {
 
                 assert.equal(proof.onetimeAddress.toString('hex').length, 130)
                 assert.equal(proof.txPublicKey.toString('hex').length, 130)
-                assert.equal(proof.mask.length, 28)
+                assert.equal(proof.encryptedAmount.length, 28)
+                assert.equal(proof.mask.length, 64)
             })
         })
 
@@ -42,7 +43,7 @@ describe('stealth', function () {
                 // create proof for a transaction 
                 var proof = sender.genTransactionProof(1000, receiver.pubSpendKey, receiver.pubViewKey)
                 // prove above information belong to receiver
-                var result = receiver.checkTransactionProof(proof.txPublicKey, proof.onetimeAddress, proof.mask)
+                var result = receiver.checkTransactionProof(proof.txPublicKey, proof.onetimeAddress, proof.encryptedAmount)
                 
                 // prove above information belong to receiver
                 assert.notEqual(result, null)

8652d965b55b66ded89f47f5d8be76af5a3b9ac1
anhnt <anhnt@tomochain.com>
2019-09-17 14:31:52 +0700

    [Chore] minify code on build

--- tools/build.js
+++ tools/build.js
@@ -1,12 +1,3 @@
-/**
- * Babel Starter Kit (https://www.kriasoft.com/babel-starter-kit)
- *
- * Copyright © 2015-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 'use strict';
 
 const fs = require('fs');
@@ -15,6 +6,7 @@ const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const minify = require("babel-minify");
 const pkg = require('../package.json');
 
 let promise = Promise.resolve();
@@ -38,10 +30,11 @@ promise = promise.then(() => del(['dist/*']));
       })),
       commonjs(),
     ],
-  })).then(bundle => bundle.write({
+  }))
+  .then(bundle => bundle.write({
     file: `dist/${format === 'cjs' ? 'index' : `index.${format}`}.js`,
     format,
-    sourceMap: true,
+    sourceMap: false,
     name: format === 'umd' ? pkg.name : undefined,
   }));
 });

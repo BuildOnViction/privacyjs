902d0d11a146509f8f9037e641be6ddcc7cefbbe
Anhnt <anhntbk08@gmail.com>
2019-10-03 14:07:05 +0700

    [Chore] move scanutxo to utils

--- test/end-to-end/1-calculate-balance.js
+++ test/end-to-end/1-calculate-balance.js
@@ -10,8 +10,7 @@ import Stealth from '../../src/stealth';
 import HDWalletProvider from "truffle-hdwallet-provider";
 import ecurve from 'ecurve';
 import crypto from '../../src/crypto';
-import UTXO from '../../src/utxo';
-
+import { scanUTXOs } from '../utils';
 const { BigInteger } = crypto;
 const ecparams = ecurve.getCurveByName('secp256k1');
 const expect = chai.expect;
@@ -32,70 +31,11 @@ var privacyContract = new web3.eth.Contract(TestConfig.PRIVACY_ABI, TestConfig.P
     gas: '1000000'
 });
 
-// todo - move to util
-function getUTXO(index) {
-    return new Promise((resolve, reject) => {
-        privacyContract.methods.getUTXO(index)
-                .call({
-                    from: WALLETS[0].address
-                })
-                .then(function (utxo) {
-                    return resolve(utxo);
-                }).catch(exception => {
-                    reject(exception);
-                })
-    });
-}
-
-const scanAllUTXO = async() => {
-    let index = 0;
-    var utxo = {};
-    var balance = 0;
-    var utxos = [];
-    do {
-        try {
-            utxo = await getUTXO(index);
-
-            if (utxo["3"] === false) {
-                let utxoInstance = new UTXO({
-                    ...utxo,
-                    "3": index
-                });
-                let isMine = utxoInstance.isMineUTXO(SENDER_WALLET.privateKey);
-    
-                if (isMine) {
-                    utxos.push(utxo);
-                }
-    
-                if (isMine && parseFloat(isMine.amount).toString() == isMine.amount ) {
-                    balance += parseFloat(isMine.amount);
-                }
-                index++;
-            } else {
-                index++;   
-            }
-            
-        } catch(exception) {
-            // console.log(exception);
-            utxo = null;
-            break;
-        }
-
-        // we can't scan all utxo, it would take minutes on testnet and days on mainet
-        // in testnet the encryption algorithm can be changed :( 
-        // if (utxos.length > 5) {
-        //     break;
-        // }
-    } while (utxo);
-
-    return balance;
-}
-
 describe('#balance', () => {
-    it('#scanAllUTXO and sum balance', (done) => {
+    it('#scanUTXOs and sum balance', (done) => {
         // scan all UTXO
-        scanAllUTXO().then((balance) => {
-            expect(balance > 0).to.be.equal(true);
+        scanUTXOs(SENDER_WALLET.privateKey, 10).then((ret) => {
+            expect(ret.balance > 0).to.be.equal(true);
             done();
         }).catch(ex => {
             done(ex);

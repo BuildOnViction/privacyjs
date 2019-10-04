6c9877097f637bad93f37f95d5ed2ab8b4d7d380
anhnt <anhnt@tomochain.com>
2019-10-04 18:01:35 +0700

    [WIP] ringct

--- src/crypto.js
+++ src/crypto.js
@@ -2,6 +2,9 @@
 // this is NATIVE LIB OF NODEJS, WOULD NOT RUN ON BROWSER
 const crypto = require('crypto');
 const ecurve = require('ecurve');
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
 
 // hack to get bigi without including it as a dep
 const ecparams = ecurve.getCurveByName('secp256k1');
@@ -23,66 +26,9 @@ export function sha256x2(buffer) {
 
 /**
  * Random a hex string with size bytes, no prefix 0x, add it yourself if needed
- * @param {number} size Number of bytes, notice a hex = 4 bit = 0.5 byte,
  * a common private key (length 64) = 32 bytes
- * @param {function} callback
  * @returns {string} Hex string without prefix 0x
  */
-export function randomHex(size, callback) {
-    const cryto = crypto || window.crypto;
-    const isCallback = (typeof callback === 'function');
-
-    if (size > 65536) {
-        if (isCallback) {
-            callback(new Error('Requested too many random bytes.'));
-        } else {
-            throw new Error('Requested too many random bytes.');
-        }
-    }
-
-    // is node
-    if (typeof cryto !== 'undefined' && cryto.randomBytes) {
-        if (isCallback) {
-            cryto.randomBytes(size, (err, result) => {
-                if (!err) {
-                    callback(null, result.toString('hex'));
-                } else {
-                    callback(err);
-                }
-            });
-        } else {
-            return cryto.randomBytes(size).toString('hex');
-        }
-
-        // is browser
-    } else {
-        let crytoLib;
-
-        if (typeof cryto !== 'undefined') {
-            crytoLib = cryto;
-        } else if (typeof window.msCrypto !== 'undefined') {
-            crytoLib = window.msCrypto;
-        }
-
-        if (crytoLib && crytoLib.getRandomValues) {
-            const randomBytes = crytoLib.getRandomValues(new Uint8Array(size));
-            const returnValue = Array.from(randomBytes).map(arr => arr.toString(16)).join('');
-
-            if (isCallback) {
-                callback(null, returnValue);
-            } else {
-                return returnValue;
-            }
-
-            // not cryto object
-        } else {
-            const error = new Error('No "cryto" object available. This Browser doesn\'t support generating secure random bytes.');
-
-            if (isCallback) {
-                callback(error);
-            } else {
-                throw error;
-            }
-        }
-    }
+export function randomHex() {
+    return ec.genKeyPair().getPrivate().toString('hex');
 }

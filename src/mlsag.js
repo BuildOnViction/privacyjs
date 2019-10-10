732dc4a110aa63e76470941a2e031104c84da558
Anhnt <anhntbk08@gmail.com>
2019-10-10 17:37:56 +0700

    [Feat] mlsag v1

--- src/mlsag.js
+++ src/mlsag.js
@@ -1,21 +1,18 @@
-
-/**
- * Multilayered linkable spontaneous ad-hoc group signatures test
- * Generate key images and ring-signature from a group (size=11)
- */
-
+// @flow
 import ecurve from 'ecurve';
-import { keccak256 } from 'js-sha3';
+import { keccak256, sha3_256 as sha3 } from 'js-sha3';
 import assert from 'assert';
 import * as _ from 'lodash';
 import { BigInteger, randomHex } from './crypto';
-import { fastHash, bconcat, hextobin } from './common';
+import { fastHash, bconcat, hextobin, d2b} from './common';
+import { fastHash, bconcat, hextobin, d2b} from './common';
+import { baseH, baseH2 } from './commitment';
 // import { soliditySha3, bintohex } from './common';
 
 const secp256k1 = ecurve.getCurveByName('secp256k1');
-// const { Point } = ecurve;
+const { Point } = ecurve;
 // const hs = hmacSha256;
-// const basePoint = secp256k1.G;
+const baseG = secp256k1.G;
 
 /**
  * Using secp256k1 to turn a public key to a point (utxo's one time address)
@@ -29,7 +26,7 @@ export const hashToPoint = (shortFormPoint) => {
     assert(shortFormPoint && shortFormPoint.length, 'Invalid input public key to hash');
     let hex = shortFormPoint.substring(2); // ignore first two bit
     while (hex) {
-        const newPoint = secp256k1.G.multiply(BigInteger.fromHex(keccak256(hex)));
+        const newPoint = baseG.multiply(BigInteger.fromHex(keccak256(hex)));
         if (secp256k1.isOnCurve(newPoint)) {
             return newPoint;
         }
@@ -38,7 +35,7 @@ export const hashToPoint = (shortFormPoint) => {
 };
 
 /**
- * A hash function for real message plus two more ring parameters for noising data
+ * A hash function for real message plus two more ring parameters for decoy data
  * this function return a value in secp256k1 prime field Zp
  * Use internal inside MLSAG only
  * @param {Buffer} message Real data we want to compress
@@ -54,10 +51,14 @@ function hashRingCT(message, Lj, Rj) {
                 Rj,
             ]),
         ),
-    );
+    ).mod(secp256k1.p);
 }
 
-// export const keyVector = (rows) => _.fill(rows, null);
+function hashToScalar(point){
+    return hashToPoint(point).getEncoded(true).toString('hex').slice(2); // ignore first byte (04, 02, 03)
+}
+
+// export const keyVector = (numberOfSendingUTXOs) => _.fill(numberOfSendingUTXOs, null);
 
 /**
  * Generate key image for single stealth pair private/public
@@ -71,8 +72,8 @@ export const keyImage = (privKey, pubKey) => {
     console.log('generating key image ', privKey);
     // const HP = [];
     // const KeyImage = [];
-    // for (let i = 0; i < rows; i++) {
-    //     HP[i] = BigInteger.fromBuffer(hs(basePoint.multiply(BigInteger.fromHex(x[i]))));
+    // for (let i = 0; i < numberOfSendingUTXOs; i++) {
+    //     HP[i] = BigInteger.fromBuffer(hs(baseG.multiply(BigInteger.fromHex(x[i]))));
     //     KeyImage[i] = HP[i].multiply(BigInteger.fromHex(x[i]));
     // }
 
@@ -104,17 +105,18 @@ export const UTXO_RING_SIZE = 11;
 export default class MLSAG {
     // sign single utxo
     // the multiple utxos version is below
-    static sign(privKey, utxos, index, targetAddress) {
-        // do i need the utxo instance or just plain data of 11 utxos - TODO give solution and explain
+    static sign(message, privKey, utxos, index, targetAddress) {
         const sender = utxos[index].getRingCTKeys(privKey);
 
-        // message should be something public
-        const message = utxos[index].getHashData(targetAddress);
-
+        // message here represent for tx_hash
+        // we concat all utxo_pubkey and hash to 32 character
+        // const message = sha3(_.map(utxos, ut => {
+        //     return ut.lfStealth.getEncoded(true).toString('hex');
+        // }).join(""));
 
         const X = BigInteger.fromHex(sender.privKey); // the private key we use for signing the message in ringCT
-        const I = keyImage(X, sender.pubKey.toString('hex')); // hash to point of current public key of
-        const HP = hashToPoint(sender.pubKey.toString('hex'));
+        const I = keyImage(X, sender.pubKey.toString('hex')); 
+        const HP = hashToPoint(sender.pubKey.toString('hex'));// hash to point of current public key of
         const n = utxos.length;
         // console.log(X);
         // console.log(I);
@@ -126,12 +128,12 @@ export default class MLSAG {
         const ci = [];
 
         // init value before generating ring
-        L[index] = secp256k1.G.multiply(alpha);
+        L[index] = baseG.multiply(alpha);
         R[index] = HP.multiply(alpha);
         ci[(index + 1) % n] = BigInteger.fromHex(
             fastHash(
                 hextobin(message),
-                +L[index].getEncoded(true).toString('hex')
+                + L[index].getEncoded(true).toString('hex')
                 + R[index].getEncoded(true).toString('hex'),
             ),
         );
@@ -140,7 +142,7 @@ export default class MLSAG {
         let isCalculatedAll = 1;
 
         while (isCalculatedAll < n) {
-            L[j] = secp256k1.G.multiply(si[(j + 1) % n]).add(
+            L[j] = baseG.multiply(si[(j + 1) % n]).add(
                 utxos[j].lfStealth.multiply(ci[j]),
             );
             R[j] = hashToPoint(utxos[j].lfStealth.getEncoded(true).toString('hex')).multiply(
@@ -164,45 +166,148 @@ export default class MLSAG {
         };
     }
 
-    static verify(utxos, I, c1, si) {
-        const rows = utxos.length; // length of noising utxo, default is 11
-        const cols = 1; // number of utxos you need to verify - now just support one input
+    static mulSign(message, privKeys, mixing, index) {
+        // input spending utxos
+        const numberOfUTXOs = privKeys.length;
 
+        // mixing lengh, here we set default as 11
+        const decoysEachRing = mixing[0].length;
+        
+        const X = [];
+        const I = [];
+        const HP = [];
         const L = [];
         const R = [];
-        // pj = ''.join(pk[0])
-        // for (var i = 0; i < rows; i++) {
-        //     pj = pj + ''.join(pk[i])
-        // }
-
+        const s = [];
         const ci = [];
-        // const HP = _.map(utxos, ut => hashToPoint(ut.lfStealth.getEncoded(true).toString('hex')));
+        
+        let pj = message;
+        for(var i = 0; i < mixing.length; i++) {
+            L.push([]);
+            R.push([]);
+            const sender = mixing[i][index].getRingCTKeys(privKeys[i]);
+            X[i] = BigInteger.fromHex(sender.privKey); // the private key we use for signing the message in ringCT
+            I[i] = keyImage(X, sender.pubKey.toString('hex')); 
+            HP[i] = _.map(mixing[i], (utxo) => {
+                pj = bconcat(pj, utxo.lfStealth.getEncoded(true));
+                return hashToPoint(utxo.lfStealth.getEncoded(true).toString('hex'));
+            });
+            s.push(_.map(new Array(mixing[i].length), () => BigInteger.fromHex(randomHex())));
+        }
 
-        ci[0] = c1;
-        let j = 0;
-        // const tohash = HP.join('');
+        for (i = 0; i < numberOfUTXOs; i++) {
+            L[i][index] = baseG.multiply(s[i][index]); //aG
+            R[i][index] = HP[i][index].multiply(s[i][index]); //aH
+        }
+            
+        let j = (index + 1) % decoysEachRing;
+        let tohash = pj;
 
-        while (j < cols) {
-            for (let i = 0; i < rows; i++) {
-                L[j] = secp256k1.G.multiply(si[j + 1]).add(
-                    utxos[j].lfStealth.multiply(ci[j]),
-                );
-                R[j] = hashToPoint(utxos[j].lfStealth.getEncoded(true).toString('hex')).multiply(
-                    si[j + 1],
-                ).add(
-                    I.multiply(ci[j]),
-                );
-                ci[j] = hashRingCT(message, L[j].getEncoded(true), R[j].getEncoded(true));
-                j += 1;
-                // tohash = tohash + L[i][j] + R[i][j];
+        for(i = 0; i < numberOfUTXOs; i++) {
+            tohash = bconcat(tohash, L[i][index].getEncoded(true), R[i][index].getEncoded(true));
+        }
+        
+        c[j] = BigInteger.fromHex(fastHash(tohash));
+
+        while (j != index){
+            tohash = pj;
+            for (i = 0; i < numberOfUTXOs; i++) {
+                L[i][j] = baseG.multiply(s[i][j]).add(
+                    mixing[i][j].lfStealth.multiply(c[j])
+                ); // Lj = sG + cxG
+                R[i][j] = HP[i][j].multiply(s[i][j]).add(
+                    I[i].multiply(c[j])
+                ); // Rj = sH + cxH
+                tohash = bconcat(tohash, L[i][j].getEncoded(true), R[i][j].getEncoded(true));
             }
+            j = (j + 1) % decoysEachRing;
+            c[j] = BigInteger.fromHex(fastHash(tohash));
+            for (i = 0; i < numberOfUTXOs; i++) {
+                s[i][index] = s[i][index].subtract(
+                    c[index].multiply(xx[i])
+                ); //si = a - c x so a = s + c x
+            }
+        }
+        return I, c[0], s
+    }
+
+    static verifyMul(message, mixing, I, c1, s ){
+        const numberOfUTXOs = mixing.length;
+
+        // mixing lengh, here we set default as 11
+        const decoysEachRing = mixing[0].length;
+        
+        const L = [];
+        const R = [];
+        let pj = message;
+        for(var i = 0; i < numberOfUTXOs; i++) {
+            L.push([]);
+            R.push([]);
+            HP[i] = _.map(mixing[i], (utxo) => {
+                pj = bconcat(pj, utxo.lfStealth.getEncoded(true));
+                return hashToPoint(utxo.lfStealth.getEncoded(true).toString('hex'));
+            });
+            s.push(_.map(new Array(mixing[i].length), () => BigInteger.fromHex(randomHex())));
+        }
+
+        const c = []; // you do an extra one, and then check the wrap around 
+        c[0] = c1;
+        let j = 0
+        while (j < decoysEachRing) {
+            let tohash = pj
+            for (i = 0; i < numberOfUTXOs; i++) {
+                L[i][j] = baseG.multiply(s[i][j]).add(
+                    mixing[i][j].lfStealth.multiply(c[j])
+                );
+                R[i][j] = HP[i][j].multiply(s[i][j]).add(
+                    I[i].multiply(c[j])
+                ); // Rj = sH + cxH
+                tohash = bconcat(tohash, L[i][j].getEncoded(true), R[i][j].getEncoded(true));
+            }
+            j = j + 1;
+            c[j] = BigInteger.fromHex(fastHash(tohash));
+        }
+        const rv = (c[0] == c[decoysEachRing])
+        
+        return rv
+    }
+
+    /**
+     * Verify the MLSAG result, this will be done in SC, we just do it ourself for unit-testing
+     * @param {utxo array} utxos list utxo, refer src/utxo.js for more detail about data structure
+     * @param {ecurve.Point} I keyImage -> represent for who actually signs
+     * @param {BigInteger} c1 first commitment for generating commiment list
+     * @param {BigInteger} si list generated private keys
+     */
+    static verify(message, utxos, I, c1, si) {
+        const numberOfDecoy = utxos.length; // length of decoy utxos, default is 11
+        const numberOfSendingUTXOs = 1; // number of utxos you need to verify - now just support one input
+        // const message = sha3(_.map(utxos, ut => {
+        //     return ut.lfStealth.getEncoded(true).toString('hex');
+        // }).join(""));
+
+        console.log(message);
+        const L = [];
+        const R = [];
+        const ci = [];
+
+        ci[0] = _.cloneDeep(c1);
+
+        for (let i = 0; i < numberOfDecoy; i++) {
+            L[j] = baseG.multiply(si[j + 1]).add(
+                utxos[j].lfStealth.multiply(ci[j]),
+            );
+            R[j] = hashToPoint(utxos[j].lfStealth.getEncoded(true).toString('hex')).multiply(
+                si[j + 1],
+            ).add(
+                I.multiply(ci[j]),
+            );
+            ci[j] = hashRingCT(message, L[j].getEncoded(true), R[j].getEncoded(true));
             j += 1;
         }
 
-        const rv = ci[0] === ci[cols];
-        // print('c', c);
-        // print('sig verifies?', rv);
-
+        const rv = ci[0] === ci[numberOfDecoy];
         return rv;
     }
+
 }

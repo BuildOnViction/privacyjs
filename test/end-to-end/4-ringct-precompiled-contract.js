082a4b9187be7547199013e0abfd18117821ed1d
anhnt <anhnt@tomochain.com>
2019-10-22 11:33:41 +0700

    [Feat] fully ringct implementation

--- test/end-to-end/4-ringct-precompiled-contract.js
+++ test/end-to-end/4-ringct-precompiled-contract.js
@@ -41,7 +41,7 @@ const privacyContract = new web3.eth.Contract(
 );
 
 describe('#ringct #verify', () => {
-    it('Successful create single ring', (done) => {
+    it('Successful create single ring-signature', (done) => {
         // TestUtils.depositNTimes(12, TOMO).then((utxos) => {
         //     const index = 3;
         const pubkeys = [[]];
@@ -128,5 +128,91 @@ describe('#ringct #verify', () => {
                 done(error);
             });
         // });
+    });
+
+    it('Successful create single commitment-ring', (done) => {
+        const pubkeys = [[]];
+        const index = 3;
+        RINGCT_DATA.NOISING_UTXOS.splice(index, 0, RINGCT_DATA.SPENDING_UTXOS[0]);
+
+        const inputUTXOS = _.map(RINGCT_DATA.NOISING_UTXOS, (ut) => {
+            const utxo = new UTXO(ut);
+            pubkeys[0].push(
+                utxo.lfStealth,
+            );
+            return utxo;
+        });
+
+        const signature = MLSAG.signCommitment(
+            SENDER_WALLET.privateKey,
+            [inputUTXOS],
+            index,
+        );
+
+        const sendingBytes = `${numberToBN(1).toString(16, 16)
+        }${numberToBN(12).toString(16, 16)
+        }${numberToBN(0).toString(16, 64)
+        }${signature.c1.toHex(32)
+        }${_.map(_.flatten(signature.s), element => element.toHex(32)).join('')
+        }${_.map(_.flatten(pubkeys), pubkey => pubkey.getEncoded(true).toString('hex')).join('')
+        }${_.map(_.flatten(signature.I), element => element.getEncoded(true).toString('hex')).join('')}`;
+
+        assert(sendingBytes.length === (16 + 16 + 64 + 64 + 12 * 64 + 66 * 12 + 66), 'Wrong calculation bytes');
+
+        expect(signature.I).not.to.equal(null);
+        expect(signature.c1).not.to.equal(null);
+        expect(signature.s).not.to.equal(null);
+
+        // self verify
+        expect(
+            MLSAG.verifyMul(
+                '',
+                [inputUTXOS],
+                signature.I,
+                signature.c1,
+                signature.s,
+            ),
+        ).to.be.equal(true);
+
+        // verify on Smart-contract
+        privacyContract.methods.VerifyRingCT(
+            Buffer.from(sendingBytes, 'hex'),
+        )
+            .send({
+                from: SENDER_WALLET.address,
+            })
+            .then((receipt) => {
+                console.log(receipt);
+                done();
+            })
+            .catch((error) => {
+                console.log(error);
+                done(error);
+            });
+        // });
+    });
+
+    it('Successful create 5 rings', (done) => {
+        done(new Error('Not implemented yet'));
+    });
+
+    it('Successful create 10 rings', (done) => {
+        done(new Error('Not implemented yet'));
+    });
+
+    it('Unsuccessful validate a ring', (done) => {
+        done(new Error('Not implemented yet'));
+    });
+
+    it('Successful create 1 ring ct', (done) => {
+        done(new Error('Not implemented yet'));
+    });
+
+    it('Should reject tx with commitment from negative value', (done) => {
+        done(new Error('Not implemented yet'));
+    });
+
+    it('Should reject tx with used-ring', (done) => {
+        done(new Error('Not implemented yet'));
     });
 });
